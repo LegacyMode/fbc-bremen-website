5cbe4fe122b68e2c01b843526d03f2369b5b775e
StevenKowalzik <stvnkwlzk@googlemail.com>
2019-10-10 22:27:30 +0200

    inital project setup

---
+++ pages/rules.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Regeln
+      </h1>
+      <ol>
+        <li>
+          Hinhocken bei Wurf der eigenen Mannschaft ist erlaubt
+          <span class="einschub"
+            >a. Hand darf das Bier erst berühren wenn das eigene Team die
+            Flasche trifft.</span
+          >
+        </li>
+        <li>
+          Immer mit der Hand oberhalb der Hüfte werfen. Hand muss von oberhalb
+          des Ellenbogens kommen.
+        </li>
+        <li>
+          Strafbier bei umgefallenen Bier, ob und wieviel ausläuft ist nicht
+          relevant
+        </li>
+        <li>Bier schützen darf nur mit dem Fuß passieren.</li>
+        <li>10m Spielfeld, also 5m + 5m</li>
+        <li>
+          Linie darf erst uebertreten werden sobald der Ball die Hand des
+          werfenden Spielers verlaesst. Das betrifft auch das Schuetzen mit dem
+          Fuss.
+        </li>
+        <li>
+          Mitglieder eines Teams müssen der Reihenfolge nach werfen (Links nach
+          Rechts oder Rechts nach Links)
+        </li>
+        <li>
+          Stopp ist, wenn Flasche steht und Ball hinter der Linie des
+          verteidigendem Team ist. Alle Spieler des Teams muessen hinter der
+          eingeen Spiellinie stehen.
+        </li>
+        <li>Münzwurf für Seitenwahl vom Schiri vor dem Spiel</li>
+        <li>
+          Wer kotzt wird ausgewechselt (nach ermessen des Schiedsrichters)
+        </li>
+        <li>
+          Foamy legitim (Sehr hoher Schaumanteil, so gut wie keine Flüssigkeit),
+          Flasche über den Kopf. Schiri hat das letzte Wort
+          <span class="einschub"
+            >a. Bei einem nicht legitimem Abschluss wird ein Strafbier an die
+            jeweilige Person verteilt</span
+          >
+        </li>
+        <li>
+          Wenn nur noch einer übrig ist, darf ein anderer Spieler des Teams
+          helfen. Dies beinhaltet nur das Laufen(Aufstellen und Ball Holen) aber
+          nicht Werfen
+        </li>
+        <li>Gewitter gibt's nicht</li>
+        <li>Biere dürfen nicht geteilt werden</li>
+        <li>Getränk muss mindestens 4,8 Prozent Volumenalkohol haben</li>
+        <li>
+          Der Schiedsrichter hat das letzte Wort, übertriebene Diskussionen mit
+          dem Schiedsrichter können mit Strafbier bestraft werden
+          <span class="einschub"
+            >a. Ebenfalls kann unsportliches Verhalten mit einem Strafbier
+            geahndet werden</span
+          >
+        </li>
+        <li>Das Bier wird gestellt von unseren Sponsoren.</li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
